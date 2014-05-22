c2199ac8a6c74691d8d8e3af90b1b10771d5f94b
David Guerette <david.guerette@gmail.com>
2014-05-22 13:41:21 -0400

    Working prototype

--- server.rb
+++ server.rb
@@ -1,27 +1,47 @@
 require 'sinatra'
 require 'pry'
+require 'csv'
 
 get '/' do
-  @tasks = File.readlines('tasks')
+  @articles_to_display =[]
+  # "Hellow, world!"
+  CSV.foreach("articles.csv", headers: true, header_converters: :symbol) do |row|
+    @articles_to_display << row.to_hash
+    # binding.pry
+    # the problem is that articles.csv is not saving as an array, but rather a list like firebase, firebase.com, description
+    # we need to make this save as an array
+  end
+  # @tasks = File.readlines('tasks')
   erb :index
 end
 
-post '/tasks' do
-  binding.pry
+post '/new_article' do
+  # binding.pry
+  article_for_csv = []
+  article_for_csv = [params["title"], params["url"], params["description"]]
+  CSV.open('articles.csv', 'a') do |file|
+    file.puts(article_for_csv)
+  end
+  # binding.pry
   # Read the input from the form the user filled out
-  task = params['task_name']
+  # @tasks = params['task_name']
 
   # Open the "tasks" file and append the task
-  File.open('tasks', 'a') do |file|
-    file.puts(task)
-  end
+  #File.open('tasks', 'a') do |file|
+   # file.puts(task)
+  # end
 
   # Send the user back to the home page which shows
   # the list of tasks
   redirect '/'
+
+  erb :index
 end
 
-# These lines can be removed since they are using the default values. They've
-# been included to explicitly show the configuration options.
+
+
+
+
+
 set :views, File.dirname(__FILE__) + '/views'
 set :public_folder, File.dirname(__FILE__) + '/public'
