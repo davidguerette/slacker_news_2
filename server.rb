edef51f721052228f620e545f12f36d3ea13646a
K <celfrost@hotmail.com>
2014-05-22 16:06:00 -0400

    Aesthetic editing and code up clean

--- server.rb
+++ server.rb
@@ -8,19 +8,13 @@ end
 
 get '/' do
   @articles_to_display =[]
-  # "Hellow, world!"
   CSV.foreach("articles.csv", headers: true, header_converters: :symbol) do |row|
     @articles_to_display.unshift(row.to_hash)
-    # binding.pry
-    # the problem is that articles.csv is not saving as an array, but rather a list like firebase, firebase.com, description
-    # we need to make this save as an array
   end
-  # @tasks = File.readlines('tasks')
   erb :index
 end
 
 post '/new_article' do
-  # binding.pry
   article_for_csv = []
   article_for_csv = [params["title"], params["url"], params["description"]]
   CSV.open('articles.csv', 'a') do |file|
