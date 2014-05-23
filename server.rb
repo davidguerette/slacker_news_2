dd43ee204e46f52cc5aea982f92279ccbe9a6d1f
David Guerette <david.guerette@gmail.com>
2014-05-23 14:36:33 -0400

    Cleaned up CSV & entered JSON parsing method

--- server.rb
+++ server.rb
@@ -1,6 +1,40 @@
 require 'sinatra'
 require 'pry'
 require 'csv'
+require 'json'
+
+
+
+def get_connection
+  if ENV.has_key?("REDISCLOUD_URL")
+      Redis.new(url: ENV["REDISCLOUD_URL"])
+    else
+      Redis.new
+    end
+end
+
+
+def find_articles
+  redis = get_connection
+  serialized_articles = redis.lrange("slacker:articles", 0, -1)
+
+  articles = []
+
+  serialized_articles.each do |article|
+    articles << JSON.parse(article, symbolize_names: true)
+  end
+
+  articles
+end
+
+def save_articles(url, title, description)
+  article = {url: url, title: title, description: description }
+
+  redis = get_connection
+
+  redis.rpush("slacker:articles", articles.to_json)
+end
+
 
 get '/comments.html' do
   erb :comments
